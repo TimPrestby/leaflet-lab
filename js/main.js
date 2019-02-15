89e8a4d86ecadc3341eb6cef1d551b51f6f53740
Timothy Prestby <tjprestby@gmail.com>
2019-02-15 13:37:10 -0600

    Finished Lesson 2 in module 5

--- js/main.js
+++ js/main.js
@@ -25,37 +25,85 @@ function createMap(){
             accessToken: 'pk.eyJ1IjoicHJlc3RpbW9qIiwiYSI6ImNqczNmYWE2bzJmNTYzeW8zOXNlMnVpOGwifQ.OrEG7gIMeP3N3sMaNY3EGw'
         }).addTo(map)};
 
-
-
-//Function to add circle markers for point
-function createPropSymbols(data,map){
+//function to convert markers to circle markers
+function pointToLayer(feature, latlng){     
     //Set marker options 
     var attribute= '2000';
-    var geojsonMarkerOptions = {
+    var options = {
         radius: 8,
         fillColor: "#293542",
         color: "#000",
         weight: 1,
         opacity: 1,
-        fillOpacity: 0.8
+        fillOpacity: 0.5
     };
 
+    //For each feature, get the value for the attribute
+    var attValue = Number(feature.properties[attribute]);
+
+    //Assign radius based on attribute value
+    options.radius=calcPropRadius(attValue);
+
+    //creates the circle marker layer
+    var layer = L.circleMarker(latlng, options);
+
+    //build popup content of the country
+    var popupContent = "<p><b>Country:</b> " + feature.properties['Country Name'] + "</p>";
+
+     //add formatted attribute to popup content string
+     var year = attribute;
+     popupContent += "<p><b>CO2 emissions in " + year + ":</b> " + (Math.floor(feature.properties[attribute]*100)/100) + " (metric tons per capita) </p>";
+    //bind the popup to the circle marker
+    //this math function rounds the values
+
+    layer.bindPopup(popupContent, {
+        offset: new L.Point(0,-options.radius)
+    });
+    //Binds the popup content to the popup object
+    //The Offset ensures that the point is placed outside the radius of the proportional symbol
+
+    //event listeners to open popup on hover
+      layer.on({
+        mouseover: function(){
+            this.openPopup();
+        },
+        //Open the popup when mousing over in desktop view
+        mouseout: function(){
+            this.closePopup();
+        },
+        //Close the popup when no longer mousing over
+
+        click: function(){
+            $("#panel").html(popupContent);
+        }
+    });
+
+    //return the circle marker to the L.geoJson pointToLayer option
+    return layer;
+};
+
+//function to calculate the radius of each proportional symbol
+function calcPropRadius(attValue) {
+    //scale factor to adjust symbol size evenly
+    var scaleFactor = 50;
+    //area based on attribute value and scale factor
+    var area = attValue * scaleFactor;
+    //radius calculated based on area
+    var radius = Math.sqrt(area/Math.PI);
+
+    return radius;
+};
+
+//Function to add circle markers for point
+function createPropSymbols(data,map){
+
     L.geoJson(data, {
-        pointToLayer: function (feature, latlng){
+        pointToLayer: pointToLayer
             //changes the point features of lat lon into point features
 
-            //For each feature, get the value for the attribute
-            var attValue = Number(feature.properties[attribute]);
-
-            console.log(feature.properties, attValue);
-
-            return L.circleMarker(latlng, geojsonMarkerOptions);
-            //return will bring the feature back to the argument and is then added on the following
-
             
-        }
-    }).addTo(map);
-};
+        }).addTo(map);
+    };
 
 //function to retrieve data and place it on the map
 function getData(map){
