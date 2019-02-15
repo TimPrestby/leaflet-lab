2044c938da959de909c89be43df7717c8b664614
Timothy Prestby <tjprestby@gmail.com>
2019-02-15 15:14:28 -0600

    Got to Lesson 4

--- js/main.js
+++ js/main.js
@@ -25,10 +25,11 @@ function createMap(){
             accessToken: 'pk.eyJ1IjoicHJlc3RpbW9qIiwiYSI6ImNqczNmYWE2bzJmNTYzeW8zOXNlMnVpOGwifQ.OrEG7gIMeP3N3sMaNY3EGw'
         }).addTo(map)};
 
+
 //function to convert markers to circle markers
-function pointToLayer(feature, latlng){     
+function pointToLayer(feature, latlng, attributes){     
     //Set marker options 
-    var attribute= '2000';
+    var attribute = attributes[0];
     var options = {
         radius: 8,
         fillColor: "#293542",
@@ -70,17 +71,47 @@ function pointToLayer(feature, latlng){
         //Open the popup when mousing over in desktop view
         mouseout: function(){
             this.closePopup();
-        },
+        }
         //Close the popup when no longer mousing over
 
-        click: function(){
-            $("#panel").html(popupContent);
-        }
+     
+
+
     });
 
     //return the circle marker to the L.geoJson pointToLayer option
     return layer;
 };
+
+//Function to resize the proportional symbols acording to attribute values
+function updatePropSymbols(map, attribute){
+    map.eachLayer(function(layer){
+        if (layer.feature && layer.feature.properties[attribute]){
+            //update the layer style and popup
+            
+            var props = layer.feature.properties;
+
+            //update each feature's radius based on new attribute values
+            var radius = calcPropRadius(props[attribute]);
+            layer.setRadius(radius);
+
+            //build popup content of the country
+            var popupContent = "<p><b>Country:</b> " + props['Country Name'] + "</p>";
+
+            //add formatted attribute to popup content string
+            var year = attribute;
+            popupContent += "<p><b>CO2 emissions in " + year + ":</b> " + (Math.floor(props[attribute]*100)/100) + " (metric tons per capita) </p>";
+            //bind the popup to the circle marker
+            //this math function rounds the values
+
+            //replace the layer popup
+            layer.bindPopup(popupContent, {
+                offset: new L.Point(0,-radius)
+            });
+        };
+        });
+    };
+
 
 //function to calculate the radius of each proportional symbol
 function calcPropRadius(attValue) {
@@ -94,11 +125,16 @@ function calcPropRadius(attValue) {
     return radius;
 };
 
-//Function to add circle markers for point
-function createPropSymbols(data,map){
+//ASK TO EXPLAIN
 
+//Function to add circle markers for point
+function createPropSymbols(data,map, attributes){
+    //will create a GeoJSOn layer via leaflet and add it to the map
     L.geoJson(data, {
-        pointToLayer: pointToLayer
+        pointToLayer: function(feature, latlng){
+            //reference an anonymous function that will allow for calling paramaters of choice
+            return pointToLayer(feature, latlng, attributes);
+        }
             //changes the point features of lat lon into point features
 
             
@@ -130,9 +166,58 @@ function createSequenceControls(map){
     $('#panel').append('<button class="skip" id="reverse">Reverse</button>');
     $('#panel').append('<button class="skip" id="forward">Skip</button>');
     //Add buttons that provide additional sequence control
+    
     $('#reverse').html('<img src="img/backward.png">');
     $('#forward').html('<img src="img/forward.png">');
     //Style the buttons
+
+    //event listener for the slider
+    $('.range-slider').on('input', function(){
+        //Step 6: get the new index value
+        var index = $(this).val();
+        updatePropSymbols(map, attributes[index]);
+
+    });
+
+    //Step 5: click listener for buttons
+    $('.skip').click(function(){
+        //get the old index value
+        var index = $('.range-slider').val();
+
+        //Step 6: increment or decrement depending on button clicked
+        if ($(this).attr('id') == 'forward'){
+            index++;
+            //Step 7: if past the last attribute, wrap around to first attribute
+            index = index > 13 ? 0 : index;
+        } else if ($(this).attr('id') == 'reverse'){
+            index--;
+            //Step 7: if past the first attribute, wrap around to last attribute
+            index = index < 0 ? 13 : index;
+        };
+
+        //Step 8: update slider
+        $('.range-slider').val(index);
+        updatePropSymbols(map, attributes[index]);
+    });
+};
+
+
+//Function to process the sequential data//
+
+function processData(data){
+var attributes = [];
+//set empty array
+
+var properties = data.features[0].properties ;
+
+for (var attribute in properties){
+    if (attribute.indexOf(20) > -1){
+        //will gett all attributes with prefix 20
+        attributes.push(attribute);
+    };
+};
+
+return attributes
 };
 
 //function to retrieve GeoJSON data and place it on the map
@@ -142,8 +227,11 @@ function getData(map){
         datatype: 'json',
         success: function(response){
                 //set callback function
-            createPropSymbols(response,map);
-            createSequenceControls(map);
+            
+            var attributes = processData(response);
+            
+            createPropSymbols(response,map, attributes);
+            createSequenceControls(map, attributes);
             
         }
     });
